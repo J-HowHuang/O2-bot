38ef179584d9cd15bf31aa6a4f532cd8cf1cd05c
J-HowHuang <howardhuang2001@gmail.com>
2021-05-28 02:26:43 +0800

    change process type to worker

--- rules.js
+++ rules.js
@@ -1,23 +1,16 @@
 module.exports = {
     zh_en_noSpace: function(msg) {
         var re = /[\u4E00-\u9FFF][a-zA-Z]|[a-zA-Z][\u4E00-\u9FFF]/;
-        if (re.test(msg)){
-            return true;
-        } else {
-            return false;
-        }
+        return re.test(msg)
     },
     zh_no_noSpace: function(msg) {
         var re = /[\u4E00-\u9FFF][0-9]|[0-9][\u4E00-\u9FFF]/;
-        if (re.test(msg)){
-            return true;
-        } else {
-            return false;
-        }
+        return re.test(msg)
     },
     too_long: function(msg) {
-        if (msg.length > 100) {
-            return true;
-        }
+        return msg.length > 100
+    },
+    threw_up: function(msg) {
+        return msg.include("我吐了")
     }
 };
