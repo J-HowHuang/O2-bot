77c3bdb48be3cc50207f82a30520601129cabfa0
J-HowHuang <howardhuang2001@gmail.com>
2021-05-28 01:18:25 +0800

    commit message 打中文 我吐了

---
+++ discord.js
@@ -0,0 +1,21 @@
+const Discord = require('discord.js');
+const { token } = require('./token.json');
+const client = new Discord.Client();
+const rules = require('./rules.js')
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+
+client.on('message', msg => {
+    if(rules.zh_en_noSpace(msg)){
+        msg.reply('哥你中英文中間沒加空格，我吐了');
+    }
+    if(rules.zh_no_noSpace(msg)){
+        msg.reply('哥你中文跟數字中間沒加空格，我吐了');
+    }
+});
+
+client.login(token);
+
---
+++ rules.js
@@ -0,0 +1,18 @@
+module.exports = {
+    zh_en_noSpace: function(msg) {
+        var re = /[\u4E00-\u9FFF][a-zA-Z]|[a-zA-Z][\u4E00-\u9FFF]/;
+        if (re.test(msg)){
+            return true;
+        } else {
+            return false;
+        }
+    },
+    zh_no_noSpace: function(msg) {
+        var re = /[\u4E00-\u9FFF][0-9]|[0-9][\u4E00-\u9FFF]/;
+        if (re.test(msg)){
+            return true;
+        } else {
+            return false;
+        }
+    },
+};
